92da19ce5bfe74a933cad247232189c6cb87504b
Ramoonus <Ramoonus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-14 16:02:06 +0000

    tagging 4.1
    git-svn-id: https://plugins.svn.wordpress.org/gmap3/tags/4.1@489912 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ gmap3.php
@@ -0,0 +1,21 @@
+<?
+/*
+Plugin Name: gmap3
+Plugin URI: http://www.ramoonus.nl/wordpress/gmap3/
+Description: A JQuery plugin to create google maps with advanced features (overlays, clusters, callbacks, events...)
+Version: 4.1
+Author: Ramoonus
+Author URI: http://www.ramoonus.nl/
+License: GPL2
+*/
+
+function rw_gmap3_init() {
+		// Google Maps API v3
+		wp_deregister_script('googlemapsapi3');
+		wp_enqueue_script('googlemapsapi3', 'http://maps.google.com/maps/api/js?sensor=false', false, '3', false); ;
+		// gmap
+		wp_deregister_script('gmap3');
+		wp_enqueue_script('gmap3', plugins_url('/js/gmap3.min.js',__FILE__), array('jquery'), '4.1');  // gimme jQuery too
+}    
+add_action('init', 'rw_gmap3_init');
+?>
